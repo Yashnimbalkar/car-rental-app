2ca7bb302ea441a8c1631da82866499311de4cf8
Yashnimbalkar <nimbalkaryash99@gmail.com>
2025-08-20 21:58:01 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Car Rental App</h1>} />
+        <Route path="/" element={<h1>Car Rental App </h1>} />
       </Routes>
     </BrowserRouter>
   );
